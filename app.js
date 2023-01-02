eab38e099b881db41ad874e989e1bda6498fb746
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-01-02 19:32:09 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,12 @@
+// console.log("Hello World !");
+
+// alert("this script works !!!")
+
+//input
+var username = prompt("What is your name ?");
+
+//processing
+var welcomeMeassage = "this script works ! " + username ;
+
+//output
+alert(welcomeMeassage);
