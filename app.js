df42bb31cb97d244813e323f12b3a5d75310d6f1
Ayushi Pandey <40225444+ayushipandey01@users.noreply.github.com>
2023-01-03 20:50:52 +0530

    minion translate done

--- app.js
+++ app.js
@@ -11,24 +11,54 @@
 // //output
 // alert(welcomeMeassage);
 
-var btnTranslate = document.querySelector("#btn-translate");
+// var btnTranslate = document.querySelector("#btn-translate");
 
 // console.log(btnTranslate);
 
-btnTranslate.addEventListener("click" , function clickEventHandler() {
-    // console.log("clicked!!!")
-    // console.log("input " , txtInput.value)
-    outputText.innerText = "nabannaa " + txtInput.value;
 
-})
 
-var txtInput = document.querySelector("#txt-input");
+// var txtInput = document.querySelector("#txt-input");
 
 // console.log(txtInput)
 
-var outputText = document.querySelector("#output");
+// var outputText = document.querySelector("#output");
 
 // outputText.innerText = "Ayushi Pandey";
 
 // console.log(outputText);
 
+
+
+var btnTranslate = document.querySelector("#btn-translate");
+
+var txtInput = document.querySelector("#txt-input");
+
+var outputText = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured" , error)
+    alert("someting wrong with serverURL. please try again after sometime!")
+}
+
+function clickHandler() {
+    var inputText = txtInput.value
+    // console.log("clicked!!!")
+    // console.log("input " , txtInput.value)
+    // outputText.innerText = "nabannaa " + txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated ;
+        outputText.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click" , clickHandler )
